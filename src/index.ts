8eba062fe079f9cf38eaba6b6b961d0125300fb0
macklinhrw <macklingaming@gmail.com>
2021-11-13 17:21:51 -0800

    test cors

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import { buildSchema } from "type-graphql";
 import { PoemResolver } from "./resolvers/PoemResolver";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import http from "http";
 import morgan from "morgan";
 // import {
 //   ApolloServerPluginLandingPageGraphQLPlayground,
@@ -23,18 +21,10 @@ const main = async () => {
     optionSuccessStatus: 200,
   }
 
-  //const httpServer = http.createServer(app);
-
   const server = new ApolloServer({
     schema: await buildSchema({
       resolvers: [PoemResolver],
     }),
-    //plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-    // plugins: [
-    //   process.env.NODE_ENV === "production"
-    //     ? ApolloServerPluginLandingPageDisabled()
-    //     : ApolloServerPluginLandingPageGraphQLPlayground(),
-    // ],
   });
 
   await server.start();
@@ -45,12 +35,5 @@ const main = async () => {
       `🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
     );
   })
-  //await new Promise((resolve) =>
-  //  //@ts-ignore types on the listen function seem to not accept the resolve object
-  //  httpServer.listen({ port: process.env.PORT }, resolve)
-  //);
-  //console.log(
-  //  `🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
-  //);
 };
 main();
