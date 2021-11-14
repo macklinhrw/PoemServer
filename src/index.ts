09c487e7e90cf2c47ff032005ee759798285840e
macklinhrw <macklingaming@gmail.com>
2021-11-13 17:20:32 -0800

    test cors

--- src/index.ts
+++ src/index.ts
@@ -17,25 +17,19 @@ const main = async () => {
   app.use(morgan("common"));
 
   // process.env.CORS_ORIGIN
-
   const corsOptions = {
     origin: '*',
     credentials: true,
     optionSuccessStatus: 200,
   }
 
-  app.all('*', (_, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    next()
-  })
-
-  const httpServer = http.createServer(app);
+  //const httpServer = http.createServer(app);
 
   const server = new ApolloServer({
     schema: await buildSchema({
       resolvers: [PoemResolver],
     }),
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    //plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     // plugins: [
     //   process.env.NODE_ENV === "production"
     //     ? ApolloServerPluginLandingPageDisabled()
@@ -44,14 +38,19 @@ const main = async () => {
   });
 
   await server.start();
-  server.applyMiddleware({ app, path: "/", cors: corsOptions });
+  server.applyMiddleware({ app, cors: corsOptions });
 
-  await new Promise((resolve) =>
-    //@ts-ignore types on the listen function seem to not accept the resolve object
-    httpServer.listen({ port: process.env.PORT }, resolve)
-  );
-  console.log(
-    `🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
-  );
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
+    );
+  })
+  //await new Promise((resolve) =>
+  //  //@ts-ignore types on the listen function seem to not accept the resolve object
+  //  httpServer.listen({ port: process.env.PORT }, resolve)
+  //);
+  //console.log(
+  //  `🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
+  //);
 };
 main();
